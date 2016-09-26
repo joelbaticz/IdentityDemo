1a51f2c6d2a7b4e0cd43739befcfcb93965f01d7
Joel Baticz <joel.baticz@gmail.com>
2016-09-26 20:48:46 +1000

    Add files via upload

--- ProjectX/src/ProjectX/Models/ProjectXUser.cs
+++ ProjectX/src/ProjectX/Models/ProjectXUser.cs
@@ -1,1 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 
+namespace ProjectX.Models
+{
+    public class ProjectXUser : IdentityUser
+    {
+        public string ExtraProp { get; set; }
+
+
+    }
+}
